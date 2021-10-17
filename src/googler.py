4b6cab9a6246e46770086e8a161f571a37832ae1
Yotam Salmon <yotam.salmon@gmail.com>
2021-10-17 16:33:30 +0300

    Add progressbar for Google search tracking

--- src/googler.py
+++ src/googler.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-
+from rich.progress import track
 from googlesearch import search
 
 
@@ -12,8 +12,9 @@ def search_terms(terms):
 # TODO: Come up with a better name
 def cross_search(terms_list):
     result_counter = Counter()
-    for terms in terms_list:
+    for terms in track(terms_list, description="Searching Google..."):
+        print()
         result = search_terms(terms)
         result_counter.update(result)
 
-    return result_counter
+    return result_counter.most_common(10)
