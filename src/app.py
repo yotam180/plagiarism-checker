49c6142da27af95438f16195d39bec1d5e41bcab
Yotam Salmon <yotam.salmon@gmail.com>
2021-10-18 14:15:08 +0300

    Full cycle - going from the server tasks to the client

--- src/app.py
+++ src/app.py
@@ -1,20 +1,52 @@
-from flask import Flask, Response, send_from_directory
+from flask import Flask, Response, send_from_directory, request
 
 import time
+
+from document_parser import Document
+from entity_selection import random_entity_select
+from googler import cross_search
+from content_scrape import get_page_contents
+from document_compare import DocumentComparer, TargetDocument
 
 
 app = Flask(__name__)
 
 
-@app.route("/process", methods=["POST"])
-def process_text():
-    print("hello")
-    def stream():
-        for i in range(3):
-            time.sleep(2)
-            yield 'data: {"a": 5}\n\n'
+def clean_newlines(x):
+    return str(x).replace("\n", "\\n")
 
-    return Response(stream(), mimetype="text/event-stream")
+
+def message(text: str) -> str:
+    return f"data: {clean_newlines(text)}\n\n"
+
+
+def do_text_processing(article: str):
+    doc = Document(article)
+    ents, weights = doc.get_weighted_entities()
+    selected_ents = random_entity_select(ents, weights)
+
+    yield message("Searching similar articles...")
+    for i, length, results in cross_search(selected_ents):
+        yield message(f"Searching similar articles... {i*100//length}%: {results}")
+
+    comparer = DocumentComparer(doc)
+
+    for url, _ in results:
+        yield message(f"Checking article at {url}")
+
+        article_text = get_page_contents(url)
+        comparer.compare_to(TargetDocument(url, article_text))
+
+    yield message(comparer.as_json)
+
+
+@app.route("/process", methods=["POST"])
+def process_text_endpoint():
+    article_to_check = str(request.data, "utf-8")
+
+    return Response(
+        do_text_processing(article_to_check),
+        mimetype="text/event-stream")
 
 
 @app.route("/<path:path>")
--- src/googler.py
+++ src/googler.py
@@ -28,15 +28,20 @@ def cross_search(terms_list):
 
     Two Google searches will be issued - "A B C" and "D E F"
 
-    The results are accumulated in a counter, and the 10 most frequent results amongst all searches are returned.
+    The results are accumulated in a counter, and the 10 most frequent results amongst all searches are returned (see below)
 
     The reason for that is the belief that random selection of different combinations of the search terms will (in high probability) yield relevant results,
         and cross-counting these results should increase the chance of selecting the most relevant pages to return.
+
+    Yields three objects:
+        - int - searches conducted so far
+        - int - total searches to be conducted
+        - List[str] - leading 10 results
     """
 
     result_counter = Counter()
-    for terms in track(terms_list, description="Searching Google..."):
+    for i, terms in enumerate(terms_list):
         result = search_terms(terms)
         result_counter.update(result)
 
-    return result_counter.most_common(10)
+        yield i + 1, len(terms_list), result_counter.most_common(10)
